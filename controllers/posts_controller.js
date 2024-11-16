fcadc2567c1c3abbacd326ca5f7fe2762033b1b4
twqDaniell <thewickedqueend@gmail.com>
2024-11-16 17:20:59 +0200

    Create post endpoint

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,1 +1,13 @@
 const postModel = require("../models/posts_model");
+
+const createPost = async (req, res) => {
+    const post = req.body;
+    try {
+      const newPost = await postModel.create(post);
+      res.status(201).send(newPost);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  };
+
+module.exports = { createPost };
